aada8fe9e744dab390f3fe835f8c0918becf1429
DemchaAV <demchaav@gmail.com>
2025-03-28 15:16:22 +0000

    Add anki folder to track

--- src/main/java/anki/data/Question.java
+++
@@ -1,6 +0,0 @@
-package anki.data;
-
-import java.util.List;
-
-public record Question(String question, String answer, List<String>tags) {
-}
--- src/main/java/anki/io/Page.java
+++
@@ -1,4 +0,0 @@
-package anki.io;
-
-public record Page(String title, String bodyText) {
-}
